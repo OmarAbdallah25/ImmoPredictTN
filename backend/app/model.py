cabef6823e94b4e2d54941e715f5c674486413a1
AyeshOmar <omarabdallah25.com@gmail.com>
2025-12-29 14:33:26 +0100

    delete

--- backend/app/model.py
+++
@@ -1,6 +0,0 @@
-import joblib
-import pandas as pd
-
-xgb_model = joblib.load("models/xgb_model.pkl")
-scaler = joblib.load("models/scaler.pkl")
-X_columns = pd.read_csv("models/X_clean.csv").columns.tolist()
--- backend/app/preprocess.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def prepare_sample(df, X_columns, scaler):
-    # Ajouter colonnes manquantes
-    for col in X_columns:
-        if col not in df.columns:
-            df[col] = 0
-    df = df[X_columns]  # Réordonner
-    return scaler.transform(df)
